1a738ffcb39b79d5aded7ea0bbee796d78d64a7e
Michael O'Grady <mogrady.professional@gmail.com>
2022-03-26 13:04:44 +0000

    Initial Commit

---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+
+/**
+ * Main Method
+ * 
+ * @author Michael
+ *
+ */
+public class Runner {
+	public static void main(String[] args) {
+// Create new Instance of GameFrame
+		GameFrame frame = new GameFrame();
+
+	}
+}
