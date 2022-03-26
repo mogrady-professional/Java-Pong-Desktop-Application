b099b65aff20440503934ea1c7395c8c5f8b3356
Michael O'Grady <mogrady.professional@gmail.com>
2022-03-26 14:07:49 +0000

    Skeleton Methods Added

---
+++ src/GamePanel.java
@@ -0,0 +1,54 @@
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	GamePanel(){
+		
+	}
+	
+	
+	public void newBall() {
+		
+	}
+	
+	public void newPaddles() {
+		
+		
+	}
+
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkCollision() {
+		
+	}
+	
+	public class AL extends KeyAdapter{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
